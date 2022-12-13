d77a9bd571ae55d586d7ac13c21227c3708b703b
Prashant <saip96260@gmail.com>
2022-12-13 13:02:52 +0530

    time optimization

--- Algorithm_2/algorithm_2.py
+++ Algorithm_2/algorithm_2.py
@@ -4,6 +4,7 @@ import time
 
 def gridMap(frame):
 
+    frame=frame[180:,:,:]
     img=frame
     lower=np.array([146,  37,  93])
     upper=np.array([157, 107, 150])
@@ -11,21 +12,21 @@ def gridMap(frame):
     imgHsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     mask=cv2.inRange(imgHsv,lower,upper)
 
-    black_block= np.zeros([18, 18], dtype = float)
-    white_block= np.ones([18, 18], dtype = float)
+    black_block= np.zeros([20, 20], dtype = float)
+    white_block= np.ones([20, 20], dtype = float)
 
-    grid_img= np.zeros([360,360],dtype=float)
+    grid_img= np.zeros([180,360],dtype=float)
 
-    grid_map=np.zeros([20,20],dtype=float)
+    grid_map=np.zeros([10,18],dtype=float)
 
 
-    r_step=18
-    c_step=18
+    r_step=20
+    c_step=20
     ini_r=0
     ini_c=0
 
-    for i in range(20):
-        for j in range(20):
+    for i in range(9):
+        for j in range(18):
             avg_list=[]
             sumx=0
             image_block=mask[ini_r:r_step,ini_c:c_step]
@@ -33,7 +34,7 @@ def gridMap(frame):
             for k in image_block:
                 sumx+=(sum(k)/len(k))
 
-            average=sumx/20
+            average=sumx/18
             avg_list.append(round(average,0))
 
             if round(average,0)>=210:
@@ -44,12 +45,12 @@ def gridMap(frame):
                 grid_map[i][j]=0
 
             ini_c=c_step
-            c_step+=18
+            c_step+=20
 
         ini_r=r_step
-        r_step+=18
+        r_step+=20
         ini_c=0
-        c_step=18
+        c_step=20
         
     return grid_img,mask,grid_map
     
---
+++ Algorithm_2/dummy.py
@@ -0,0 +1,37 @@
+from collections import Counter
+import cv2
+import time
+from algorithm_2 import *
+
+cap=cv2.VideoCapture("y2mate.com - Feature Space Optimization for Semantic Video Segmentation  CityScapes Demo 02_1080p.mp4")
+fps_list=[]
+prev_frame_time=0
+new_frame_time=0
+font=cv2.FONT_HERSHEY_COMPLEX
+
+while(cap.isOpened()):
+    ret,frame=cap.read()
+    if not ret:
+        break
+    
+    frame=cv2.resize(frame,(360,360))
+    grid_img,mask,grid_map=gridMap(frame)
+
+    new_frame_time=time.time()
+
+    fps=1/(new_frame_time-prev_frame_time)
+    prev_frame_time=new_frame_time
+    fps=str(int(fps))
+    fps_list.append(int(fps))
+    cv2.putText(frame, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
+    cv2.imshow('frame', frame)
+    cv2.imshow("Grid Image",grid_img)
+    cv2.imshow("Mask Image",mask)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+ 
+cap.release()
+cv2.destroyAllWindows()
+
+print(dict(Counter(fps_list)))
+print(sum(fps_list)/len(fps_list))
